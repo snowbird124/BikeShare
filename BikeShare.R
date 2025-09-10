585c95a16df0a8d1e55f6508c2d05040d1f41aa9
snowbird124 <layne.larson12@gmail.com>
2025-09-10 15:06:51 -0600

    Model and predictions
    made a qmd file, moved everything over, and did a simple lm to get predictions

--- BikeShare.R
+++ BikeShare.R
@@ -1,2 +1,25 @@
 library(tidyverse)
 library(tidymodels)
+library(vroom)
+library(patchwork)
+
+train <- vroom('/Users/laynelarson12/Documents/GitHub/STAT 348/BikeShare/train.csv')
+
+
+weather <- ggplot(data = train, aes(x = weather)) +
+  geom_bar()
+
+temp <- ggplot(data = train, aes(x = temp, y = count)) +
+  geom_point() +
+  geom_smooth(se = FALSE)
+
+plot3 <- ggplot(data = train, aes(x = humidity, y = count)) +
+  geom_point() +
+  geom_smooth(se = FALSE)
+
+plot4 <- ggplot(data = train, aes(x = season, y = count)) +
+  geom_point()
+
+(weather + temp) / (plot3 + plot4)
+
+
